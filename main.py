750d13bd56dfe450e56699f788b1989b65408d1d
Aayush Mohan <66319691+AayushMohan@users.noreply.github.com>
2022-11-05 13:31:49 +0000

    Added: Math functions

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-# Data Types: List of data types in python
-int
-float
-bool
-str
-list
-tuple
-set
-dict
+# Fundamental Data Types: List of data types in python
+# int & float
+# print(type(6))
+# print(type(2 - 4))
+# print(type(2 * 4))
+# print(type(2 / 4))  #.5
 
-name = input("what is your name?")
-print("hello " + name)
+# print(2 ** 3)
+# print(5 // 4)
+# print(6 % 4)
+
+# Math Functions
+print(round(3.9))
+
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+# Classes -> custom types
+
+#Specialized Data types
+# name = input("what is your name?")
+# print("hello " + name)
