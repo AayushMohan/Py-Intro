d8ce45ba2cefd673a1d0c89c4c51b9c50a560002
Aayush Mohan <66319691+AayushMohan@users.noreply.github.com>
2022-11-30 14:52:03 +0000

    Added List Slicing

--- main.py
+++ main.py
@@ -153,6 +153,11 @@ set
 # #Lists
 # #List in python is just like Arrays in other programming languages
 
-# amazon_cart= ['notebooks', 'shades']
+# amazon_cart = ['notebooks', 'shades', 'toys', 'grapes']
 
+# amazon_cart[0] = 'laptop'
+# new_cart = amazon_cart[:]
+# new_cart[0] = 'gum'
+
+# print(new_cart)
 # print(amazon_cart[0])
